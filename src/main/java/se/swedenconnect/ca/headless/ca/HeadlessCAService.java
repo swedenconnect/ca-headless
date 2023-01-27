effc4422ff87582753a21283592d412e12025e2b
Stefan Santesson <stefan@aaa-sec.com>
2023-01-27 18:29:54 +0100

    Update HeadlessCAService.java
    Validation of AKI in cert requests

--- src/main/java/se/swedenconnect/ca/headless/ca/HeadlessCAService.java
+++ src/main/java/se/swedenconnect/ca/headless/ca/HeadlessCAService.java
@@ -18,9 +18,18 @@ package se.swedenconnect.ca.headless.ca;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.asn1.x509.AuthorityKeyIdentifier;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.SubjectKeyIdentifier;
 import org.bouncycastle.cert.X509CertificateHolder;
+
+import se.swedenconnect.ca.engine.ca.issuer.CertificateIssuanceException;
 import se.swedenconnect.ca.engine.ca.issuer.CertificateIssuerModel;
 import se.swedenconnect.ca.engine.ca.models.cert.CertNameModel;
+import se.swedenconnect.ca.engine.ca.models.cert.CertificateModel;
+import se.swedenconnect.ca.engine.ca.models.cert.extension.ExtensionModel;
+import se.swedenconnect.ca.engine.ca.models.cert.extension.impl.simple.AuthorityKeyIdentifierModel;
 import se.swedenconnect.ca.engine.ca.models.cert.impl.DefaultCertificateModelBuilder;
 import se.swedenconnect.ca.engine.ca.repository.CARepository;
 import se.swedenconnect.ca.engine.revocation.crl.CRLIssuerModel;
@@ -31,7 +40,10 @@ import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.cert.CertificateEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * The implementation of a CA instance
@@ -61,4 +73,52 @@ public class HeadlessCAService extends AbstractBasicCA {
       .ocspServiceUrl(StringUtils.isBlank(ocspResponderUrl) ? null : ocspResponderUrl);
     return certModelBuilder;
   }
+
+  @Override
+  public X509CertificateHolder issueCertificate(final CertificateModel certificateModel)
+    throws CertificateIssuanceException {
+    checkIncomingRequest(certificateModel);
+    final X509CertificateHolder certificate = this.getCertificateIssuer().issueCertificate(certificateModel);
+    try {
+      getCaRepository().addCertificate(certificate);
+    }
+    catch (final IOException e) {
+      throw new CertificateIssuanceException(e);
+    }
+    return certificate;
+  }
+
+  private void checkIncomingRequest(CertificateModel certificateModel) throws CertificateIssuanceException {
+    List<ExtensionModel> extensionModels = certificateModel.getExtensionModels();
+    List<ExtensionModel> updatedExtensionModels = new ArrayList<>();
+    for (ExtensionModel extensionModel : extensionModels) {
+      if (extensionModelMatch(extensionModel, Extension.authorityKeyIdentifier)) {
+        updatedExtensionModels.add(checkAki(extensionModel));
+      } else {
+        updatedExtensionModels.add(extensionModel);
+      }
+    }
+    certificateModel.setExtensionModels(updatedExtensionModels);
+  }
+
+  private boolean extensionModelMatch(ExtensionModel extensionModel, ASN1ObjectIdentifier extOid)
+    throws CertificateIssuanceException {
+    return extensionModel.getExtensions().stream()
+      .anyMatch(extension -> extension.getExtnId().equals(extOid));
+  }
+
+  private ExtensionModel checkAki(ExtensionModel extensionModel) throws CertificateIssuanceException {
+    AuthorityKeyIdentifier aki = AuthorityKeyIdentifier.getInstance(extensionModel.getExtensions().get(0).getParsedValue());
+    byte[] requestedKeyIdentifier = aki.getKeyIdentifier();
+    Extension issuerSkiExt = getCaCertificate().getExtension(Extension.subjectKeyIdentifier);
+    Objects.requireNonNull(issuerSkiExt, "Issuer subject key identifier must not be null");
+    SubjectKeyIdentifier issuerSki = SubjectKeyIdentifier.getInstance(issuerSkiExt.getParsedValue());
+    if (!Arrays.equals(issuerSki.getKeyIdentifier(), requestedKeyIdentifier)) {
+      log.warn("Requested AKI does not match Issuer SKI. Changing to Issuer SKI match");
+      aki = new AuthorityKeyIdentifier(issuerSki.getKeyIdentifier());
+      return new AuthorityKeyIdentifierModel(aki);
+    }
+    return extensionModel;
+  }
+
 }
